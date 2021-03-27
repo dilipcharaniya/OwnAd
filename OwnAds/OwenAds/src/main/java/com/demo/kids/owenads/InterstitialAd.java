15358e83c8ae95dddfb3c1b6f04d6951e0ca48a1
Dilip Charaniya <71262960+dilipcharaniya@users.noreply.github.com>
2021-03-27 14:06:06 +0530

    OwnAd
    add

---
+++ OwnAds/OwenAds/src/main/java/com/demo/kids/owenads/InterstitialAd.java
@@ -0,0 +1,88 @@
+package com.demo.kids.owenads;
+
+public class InterstitialAd {
+
+    private String iconUrl;
+    private String appTitle;
+    private String appDesc;
+    private String largeImageUrl;
+    private String packageNameOrUrl;
+    private String ctaText;
+    private String price;
+    private String rating;
+
+    public InterstitialAd(String iconUrl, String appTitle, String appDesc, String largeImageUrl, String packageNameOrUrl, String ctaText, String price, String rating) {
+        this.iconUrl = iconUrl;
+        this.appTitle = appTitle;
+        this.appDesc = appDesc;
+        this.largeImageUrl = largeImageUrl;
+        this.packageNameOrUrl = packageNameOrUrl;
+        this.ctaText = ctaText;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public String getAppTitle() {
+        return appTitle;
+    }
+
+    public void setAppTitle(String appTitle) {
+        this.appTitle = appTitle;
+    }
+
+    public String getAppDesc() {
+        return appDesc;
+    }
+
+    public void setAppDesc(String appDesc) {
+        this.appDesc = appDesc;
+    }
+
+    public String getLargeImageUrl() {
+        return largeImageUrl;
+    }
+
+    public void setLargeImageUrl(String largeImageUrl) {
+        this.largeImageUrl = largeImageUrl;
+    }
+
+    public String getPackageNameOrUrl() {
+        return packageNameOrUrl;
+    }
+
+    public void setPackageNameOrUrl(String packageNameOrUrl) {
+        this.packageNameOrUrl = packageNameOrUrl;
+    }
+
+    public String getCtaText() {
+        return ctaText;
+    }
+
+    public void setCtaText(String ctaText) {
+        this.ctaText = ctaText;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
---
+++ OwnAds/OwenAds/src/main/java/com/demo/kids/owenads/MyAd.java
@@ -0,0 +1,47 @@
+package com.demo.kids.owenads;
+
+public class MyAd {
+    private String appIcon;
+    private String appName;
+    private String appDescription;
+    private String url;
+
+    public MyAd(String appIcon, String appName, String appDescription, String url) {
+        this.appIcon = appIcon;
+        this.appName = appName;
+        this.appDescription = appDescription;
+        this.url = url;
+    }
+
+    public String getAppIcon() {
+        return appIcon;
+    }
+
+    public void setAppIcon(String appIcon) {
+        this.appIcon = appIcon;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppDescription() {
+        return appDescription;
+    }
+
+    public void setAppDescription(String appDescription) {
+        this.appDescription = appDescription;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
